d41bc57c9e99ff89401013c20f3edb1af9ab707e
Thusaraka Palliyeguru <87273240+box0de0ex@users.noreply.github.com>
2024-01-24 18:31:45 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-icon').addEventListener('click', function () {
+    var mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.classList.toggle('active');
+});
+
