4a6be32ada85d641b1ef056172733cc8a5e0e615
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-07-29 01:41:40 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
                 </p>
                 <div className="flex gap-4">
                   <Link
-                    href="https://drive.google.com/file/d/1KykMrUPYXU4Yf3_zqw1QTNZxW44Qyv5s/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1Syf75n496WDCIgrUs95f0XWf5fSZVToj/view?usp=sharing"
                     className="inline-flex items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
                     prefetch={false}
                   >
